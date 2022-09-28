dd13ea42101d8ab194c51ffb3ec31d0fb39dfcc4
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-09-28 16:22:00 -0500

    Control Statements and Loops: fizzbuzz exercise completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,32 +1,26 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        // While Loop
+        // Fizzbuzz
         //
-        // Create an integer variable i with a value of 5.
-        // Create a while loop that runs so long as i is less than or equal to 15
-        // Each loop iteration, output the current value of i, then increment i by one.
-        // int i = 5;
-        // while (i <= 15) {
-            // System.out.println(i);
-            // i++;
-        // }
-        for(int i = 5; i <= 15; i++) {
+        // One of the most common interview questions for entry-level programmers is the FizzBuzz test.
+        // Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+        //
+        // Write a program that prints the numbers from 1 to 100.
+        // For multiples of three: print “Fizz” instead of the number.
+        // For the multiples of five: print “Buzz”.
+        // For numbers which are multiples of both three and five: print “FizzBuzz”.
+        for (int i = 0; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
             System.out.println(i);
-        }
-
-        // Do While
-        //
-        // Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
-        // Alter your loop to count backwards by 5's from 100 to -10.
-        // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000.
-        // long j = 2;
-        // do {
-        //     System.out.println(j);
-        //     j *= j;
-        // } while (j <= 1000000);
-        for (long j = 2; j < 1000000; j *= j) {
-            System.out.println(j);
         }
     }
 }
