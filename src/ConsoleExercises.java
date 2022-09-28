eed180a8b25ea5fccf2cc95265c22b3a4a6354e3
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-09-28 11:54:01 -0500

    Console IO: exercise w/ bonus tested and completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,41 +1,40 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConsoleExercises {
 
     public static void main(String[] args) {
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %.2f%n", pi);
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f%n", pi);
 
         // Prompt a user to enter an integer and store that value in an int variable using the nextInt method.
         // What happens if you input something that is not an integer?
         // - Compiler returns you an error when input is something other than integer.
         Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter an integer: ");
-//        int userInput = scanner.nextInt();
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
 
         // Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
         // What happens if you enter less than 3 words?
         // - It just keeps going to a new line until a character is pressed, and then it allows you to go to the next prompt.
         // What happens if you enter more than 3 words?
         // - It'll ignore anything after the third word of your input.
-//        System.out.print("Enter your first word: ");
-//        String userInput1 = scanner.next();
-//        System.out.print("Enter your second word: ");
-//        String userInput2 = scanner.next();
-//        System.out.print("Enter your third word: ");
-//        String userInput3 = scanner.next();
-//        System.out.printf("Your first word was: %s%nYour second word was: %s%nYour third word was: %s%n", userInput1, userInput2, userInput3);
-//
-//        scanner.nextLine();
+        System.out.print("Enter your first word: ");
+        String userInput1 = scanner.next();
+        System.out.print("Enter your second word: ");
+        String userInput2 = scanner.next();
+        System.out.print("Enter your third word: ");
+        String userInput3 = scanner.next();
+        System.out.printf("Your first word was: %s%nYour second word was: %s%nYour third word was: %s%n", userInput1, userInput2, userInput3);
+
+        scanner.nextLine();
 
         // Prompt a user to enter a sentence, then store that sentence in a String variable using the next method.
         // Then, display that sentence back to the user.
         // Do you capture all the words?
         // - No, I only capture the first word.
-//        System.out.print("Enter a sentence: ");
-//        String userStr = scanner.nextLine();
-//        System.out.printf("Your sentence is: %s%n", userStr);
+        System.out.print("Enter a sentence: ");
+        String userStr = scanner.nextLine();
+        System.out.printf("Your sentence is: %s%n", userStr);
 
         // Prompt the user to enter values of length and width of a classroom at Codeup (or your room if virtual).
         // Display the area and perimeter of that classroom.
