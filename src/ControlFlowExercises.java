5cec4d0747cdd60c586e8665eee952b7c1a2c426
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-09-29 10:02:18 -0500

    ControlFlowExercise: convert numbers to grade exercise completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,29 +2,49 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
 
-    public static void table(int num) {
-        System.out.println();
-        System.out.println("Here is your table!");
-        System.out.println();
-        System.out.println("number | squared | cubed");
-        System.out.println("------ | ------- | -----");
-        for (int i = 1; i <= num; i++) {
-            System.out.printf("%d\t   | %d\t     | %d\t\n", i, i * i, i * i * i);
-        }
-    }
-
     public static void main(String[] args) {
-        // Display a table of powers.
+        // Convert given number grades into letter grades.
         //
-        // Prompt the user to enter an integer.
-        // Display a table of squares and cubes from 1 to the value entered.
-        // Ask if the user wants to continue.
-        // Assume that the user will enter valid data.
-        // Only continue if the user agrees to.
-        Scanner sc = new Scanner(System.in);
+        // Prompt the user for a numerical grade from 0 to 100.
+        // Display the corresponding letter grade.
+        // Prompt the user to continue.
+        // Assume that the user will enter valid integers for the grades.
+        // The application should only continue if the user agrees to.
+        // Grade Ranges:
+        //
+        // A : 100 - 88
+        // B : 87 - 80
+        // C : 79 - 67
+        // D : 66 - 60
+        // F : 59 - 0
+        boolean keepGoing = false;
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a numerical grade from 0 to 100: ");
+            int userNum = sc.nextInt();
+            if (userNum >= 88 && userNum <= 100) {
+                System.out.println("A");
+                System.out.println();
+            } else if (userNum >= 80 && userNum <= 87) {
+                System.out.println("B");
+                System.out.println();
+            } else if (userNum >= 67 && userNum <= 79) {
+                System.out.println("C");
+                System.out.println();
+            } else if (userNum >= 60 && userNum <= 66) {
+                System.out.println("D");
+                System.out.println();
+            } else if (userNum >= 0 && userNum <= 59) {
+                System.out.println("F");
+                System.out.println();
+            }
+            sc.nextLine();
+            System.out.print("Would you like to continue? [y, n] ");
+            String userContinue = sc.nextLine();
 
-        System.out.print("Enter an integer: ");
-        int userNum = sc.nextInt();
-        table(userNum);
+            if (userContinue.equalsIgnoreCase("y")) {
+                keepGoing = true;
+            }
+        } while (keepGoing);
     }
 }
